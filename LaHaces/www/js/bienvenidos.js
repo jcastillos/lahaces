53fa2bb11e3cad4c7bb281d4c29d7ff3ca34c9a4
Jaime Castillo <jcastillos@pucp.pe>
2016-11-07 00:00:31 -0500

    pusheo cutter

--- LaHaces/www/js/bienvenidos.js
+++ LaHaces/www/js/bienvenidos.js
@@ -3,14 +3,16 @@ $(document).ready(function(){
     $.ajax({
         url: "http://127.0.0.1:8080/event/", success: function(json){
             eventos = json.eventos;
-            alert('lacagada');
+            alert("holaaa");
             for(i = 0; i < eventos.length; i++){
-                        $("#lista").append('<td id="id">'+eventos[i].name+'</h4>'
+                        $("#lista").append('<tr action="http://127.0.0.1:8080/event/ method="get">'
+                        + '<td id="id">'+eventos[i].name+'</h4>'
                         +'<td id="nombre">'+eventos[i].name+'</p>'
                         +'<td id="fecha">'+eventos[i].date+'</p>'
                         +'<td id="lugar">'+eventos[i].place+'</p>'
                         +'<td><button type="button" class="btn btn-block btn-primary"  id="ver">Ver</button></td>'
-                        +'<td><button type="button" class="btn btn-block btn-sucess" id ="editar">Editar</button></td>');
+                        +'<td><button type="button" class="btn btn-block btn-sucess" id ="editar">Editar</button></td>'
+                        +'</tr>');
             }
         }
     });
--- LaHaces/www/js/buscar.js
+++ LaHaces/www/js/buscar.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
       
-    $( "#buscar_input" ).keypress(function(e) {
+    $("#buscar_input" ).keypress(function(e) {
         //console.log( "Handler for .keypress() called." );
         var nombre_evento = $( "#buscar_input" ).val();
         var nombre_sin_hashtag=nombre_evento.split("#")[1];
@@ -12,7 +12,7 @@ $( document ).ready(function() {
                     eventos = json.eventos;
                     for(i = 0; i < eventos.length; i++){
                         $("#info_eventos").append('<div id="evento'+eventos[i].id+'">'
-                        +'<form action="http://10.101.49.72:8080/event/'+eventos[i].id+'" method="get" class="list-group-item">'
+                        +'<form action="http://127.0.0.1:8080/event/'+eventos[i].id+'" method="get" class="list-group-item">'
                         //+'<a type="submit" value="Submit" class="list-group-item">'
                         +'<h4 class="list-group-item-heading">'+eventos[i].name+'</h4>'
                         +'<p class="list-group-item-text">Fecha: '+eventos[i].date+'</p>'
