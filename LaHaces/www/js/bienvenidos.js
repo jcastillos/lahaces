899b5b9d3d251d092fc4261c07c7004fe6f69175
Jaime Castillo <jcastillos@pucp.pe>
2016-11-07 00:12:11 -0500

    a

--- LaHaces/www/js/bienvenidos.js
+++ LaHaces/www/js/bienvenidos.js
@@ -6,10 +6,10 @@ $(document).ready(function(){
             alert("holaaa");
             for(i = 0; i < eventos.length; i++){
                         $("#lista").append('<tr action="http://127.0.0.1:8080/event/ method="get">'
-                        + '<td id="id">'+eventos[i].name+'</h4>'
-                        +'<td id="nombre">'+eventos[i].name+'</p>'
-                        +'<td id="fecha">'+eventos[i].date+'</p>'
-                        +'<td id="lugar">'+eventos[i].place+'</p>'
+                        +'<td>'+eventos[i].id+'</h4>'
+                        +'<td>'+eventos[i].name+'</p>'
+                        +'<td>'+eventos[i].date+'</p>'
+                        +'<td>'+eventos[i].place+'</p>'
                         +'<td><button type="button" class="btn btn-block btn-primary"  id="ver">Ver</button></td>'
                         +'<td><button type="button" class="btn btn-block btn-sucess" id ="editar">Editar</button></td>'
                         +'</tr>');
