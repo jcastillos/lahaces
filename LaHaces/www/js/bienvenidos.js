2962dadf1132fe897d9cd64bdd523003d1a50e09
Jaime Castillo <jcastillos@pucp.pe>
2016-11-07 00:45:17 -0500

    a

--- LaHaces/www/js/bienvenidos.js
+++ LaHaces/www/js/bienvenidos.js
@@ -3,18 +3,21 @@ $(document).ready(function(){
     $.ajax({
         url: "http://127.0.0.1:8080/event/", success: function(json){
             eventos = json.eventos;
-            alert("holaaa");
             for(i = 0; i < eventos.length; i++){
                         $("#lista").append('<tr action="http://127.0.0.1:8080/event/ method="get">'
                         +'<td>'+eventos[i].id+'</h4>'
                         +'<td>'+eventos[i].name+'</p>'
                         +'<td>'+eventos[i].date+'</p>'
                         +'<td>'+eventos[i].place+'</p>'
-                        +'<td><button type="button" class="btn btn-block btn-primary"  id="ver">Ver</button></td>'
-                        +'<td><button type="button" class="btn btn-block btn-sucess" id ="editar">Editar</button></td>'
+                        +'<td><button type="button" class="btn btn-block btn-primary" class="ver" value="'+eventos[i].id+'">Ver</button></td>'
                         +'</tr>');
             }
         }
+    });
+
+    $(".ver").click(function() {
+        var id=$("#ver").val();
+
     });
 
 });
